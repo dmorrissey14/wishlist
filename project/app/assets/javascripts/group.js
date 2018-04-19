e659aff56111fb6ab058b4260134c2f65b4df5de
zlister <zlister@bu.edu>
2018-04-19 19:02:14 -0400

    Style updates

--- project/app/assets/javascripts/group.js
+++ project/app/assets/javascripts/group.js
@@ -1,5 +1,5 @@
 document.addEventListener("turbolinks:load", function() {
-  $(".group-name").on("click", function(){
+  $(".collapse-target").on("click", function(){
     $(this).parents(".group-header").children(".group-details").toggleClass("item-hide");
   });
 });
