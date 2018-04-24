1eea543ecd2a926651e0356776287f63b9cf43e6
Ed Ryan <36022622+EJRyan89@users.noreply.github.com>
2018-04-23 23:12:32 -0400

    Add database seeding for list performance testing

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -8,10 +8,27 @@
 #  Character.create(name: 'Luke', movie: movies.first)
 
 case Rails.env
+
+# Do development environment specific seeding here.
 when 'development'
-  # Do development environment specific seeding here.
+  # Common development account
   User.create(email: 'dev@thewishlist.com', password: 'Th3W1shL1st',
               first_name: 'WishList', last_name: 'Dev')
+
+  # Common development account pre-configured for manual performance tests
+  perf_user = User.create(email:      'perftest@thewishlist.com',
+                          password:   'Th3W1shL1st',
+                          first_name: 'WishList',
+                          last_name:  'Performance')
+  perf_list = List.create(name: 'Performance Test List Items', owner: perf_user)
+  test_item_name_format = 'TestItem%<index>s'
+  (0..99).each do |i|
+    ListItem.create(description: format(test_item_name_format, index: i),
+                    comments:    format(test_item_name_format, index: i),
+                    site_link:   'http://localhost:3000',
+                    image_link:  'http://via.placeholder.com/350x150',
+                    list:        perf_list)
+  end
   # when 'test'
   #   # Do test environment specific seeding here.
   # when 'production'
